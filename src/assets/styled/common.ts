40e719289d6b1ea26d4a99baed36fd43c588178f
bpthess <bpthess@naver.com>
2022-10-06 03:48:16 +0900

    First Commit

---
+++ src/assets/styled/common.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  width: 100%;
+`;
+
+export const Wrapper = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+`;
+
+export const OnDirectButton = styled.button`
+  width: 100px;
+  height: 50px;
+  background-color: #ccc;
+  border-radius: 8px;
+  margin: 10px 0;
+`;
